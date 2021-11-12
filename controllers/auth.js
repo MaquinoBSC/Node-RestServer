dc49b5efb140d0022188b28055bce76776037c21
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-11 22:40:05 -0600

    Generacion del token

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,50 @@
 const { request, response } = require("express");
+const bcryptjs= require('bcryptjs');
+const Usuario= require('../models/usuario');
+const { generarJWT } = require("../helpers/generarJWT");
 
-const login= (reqe= request, res= response)=> {
 
+const login= async(req= request, res= response)=> {
+    const { correo, password }= req.body;
 
-    res.json({
-        msg: "Login OK",
-    })
+    try {
+        //Verificar si el correo existe
+        const usuario= await Usuario.findOne({ correo });
+        if(!usuario){
+            return res.status(400).json({
+                msg: "Usuario/Password no son correctos -correo"
+            })
+        }
+
+        //Verificar si el usuario esta activo en BD
+        if(!usuario.estado){
+            return res.status(400).json({
+                msg: "Usuario/Password no son correctos -estado"
+            })
+        }
+
+        //Verificar la contraseña
+        const validPassword= bcryptjs.compareSync(password, usuario.password);
+        if(!validPassword){
+            return res.status(400).json({
+                msg: "Usuario/Password no son correctos -password"
+            });
+        }
+
+        //Generar el KWT
+        const token= await generarJWT( usuario.id );
+        
+        res.json({
+           usuario,
+           token,
+        });
+
+    } catch (error) {
+        console.log(error);
+        return res.status(500).json({
+            msg: "Hable con el administrador"
+        })
+    }
 }
 
 
