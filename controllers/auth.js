762d525fc096edeb70801d21e0785075ddfcfa26
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-18 13:37:53 -0600

    Generacion de endpoint para manejar la autenticacion de google

--- controllers/auth.js
+++ controllers/auth.js
@@ -48,6 +48,17 @@ const login= async(req= request, res= response)=> {
 }
 
 
+const googleSignIn= async (req= request, res= response)=> {
+    const { id_token }= req.body;
+
+    res.json({
+        msg: "Todo bien",
+        id_token,
+    });
+}
+
+
 module.exports= {
     login,
+    googleSignIn,
 }
