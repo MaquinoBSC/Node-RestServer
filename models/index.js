f7403ed5efee8d750cdf43e953d7dc030ec0422e
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-25 11:32:31 -0600

    Crear modelo para Productos

--- models/index.js
+++ models/index.js
@@ -1,4 +1,5 @@
 const Categoria= require('./categoria');
+const Producto= require('./producto');
 const Role= require('./role');
 const Server= require('./server');
 const Usuario= require('./usuario');
@@ -7,7 +8,8 @@ const Usuario= require('./usuario');
 
 module.exports= {
     Categoria,
+    Producto,
     Role,
     Server,
-    Usuario
+    Usuario,
 }
