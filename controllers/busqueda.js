eadd3b9f47283b781222f9536fd1d9353d505ac0
MaquinoBSC <martinare.ipn@gmail.com>
2021-11-29 20:32:15 -0600

    Iniciar busqueda a la base de datos

--- controllers/busqueda.js
+++ controllers/busqueda.js
@@ -1,14 +1,56 @@
 const { request, response } = require("express");
+const { ObjectId }= require('mongoose').Types;
+const { Usuario, Categoria, Producto }= require('../models')
 
+
+const coleccionesPermitidas= [
+    'usuarios',
+    'categoria',
+    'productos',
+    'roles'
+];
+
+
+const buscarUsuarios= async ( termino= '', res= response)=> {
+    const isMongoID= ObjectId.isValid(termino);
+
+    if(isMongoID){
+        const usuario= await Usuario.findById(termino);
+        res.json({
+            results: ( usuario ) ? [ usuario ] : [],
+        });
+    }
+}
 
 const buscar= (req= request, res= response)=> {
     const { coleccion, termino }= req.params;
 
-    res.status(200).json({
-        msg: "buscando...",
-        coleccion,
-        termino
-    });
+    if(!coleccionesPermitidas.includes(coleccion)){
+        return res.status(400).json({
+            msg: `Las colecciones permitidas son: ${ coleccionesPermitidas }`
+        });
+    }
+
+
+    switch (coleccion) {
+        case "usuarios":
+            buscarUsuarios(termino, res);
+            break;
+        
+        case "categoria":
+            
+            break;
+
+        case "productos":
+            
+            break;
+    
+        default:
+            res.status(500).json({
+                'msg': "Se me olvido hacer esta busqueda"
+            })
+            break;
+    }
 }
 
 
