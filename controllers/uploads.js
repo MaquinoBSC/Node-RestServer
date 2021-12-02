295f7f7d595b19117b4953917baf045ec5345078
MaquinoBSC <martinare.ipn@gmail.com>
2021-12-02 11:16:11 -0600

    cmbiar nombre del archivo, crear carpeta destino y crear helper subirArchivo

--- controllers/uploads.js
+++ controllers/uploads.js
@@ -1,8 +1,9 @@
-const path= require('path');
 const { request, response } = require("express");
+const { subirArchivo } = require("../helpers");
 
 
-const cargarArchivo= (req= request, res= response)=> {
+
+const cargarArchivo= async (req= request, res= response)=> {
 
     if (!req.files || Object.keys(req.files).length === 0) {
         res.status(400).json({
@@ -18,30 +19,19 @@ const cargarArchivo= (req= request, res= response)=> {
         return;
     }
 
-    const { archivo } = req.files;
-    const nombreCortado= archivo.name.split('.');
-    const extension= nombreCortado[ nombreCortado.length - 1 ];
-
-    //validar la extension
-    const extensionesValidas= ['png', 'jpg', 'jpeg', 'gif'];
-    if(!extensionesValidas.includes(extension)){
-        return res.status(400).json({
-            msg: `La extension ${extension} no es permitida, ${extensionesValidas}`
+    try {
+        const nombreArchivo= await subirArchivo(req.files, undefined, 'imgs');
+        
+        res.json({
+            path: nombreArchivo
         });
+
+    } catch (error) {
+        res.status(400).json({
+            msg: error
+        })
     }
 
-
-    const uploadPath = path.join(__dirname, '../uploads/', archivo.name);
-
-    archivo.mv(uploadPath, (err)=> {
-        if (err) {
-            return res.status(500).json({ err });
-        }
-
-        res.json({
-            msg: 'File uploaded to ' + uploadPath
-        });
-    });
 }
 
 
